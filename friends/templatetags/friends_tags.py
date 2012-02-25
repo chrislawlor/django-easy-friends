2b162137d310dfac4a17a3f798bf79df48e31f8a
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2012-02-26 00:36:53 +0100

    friendship_status changed to are_friends, now it returns boolean value

--- friends/templatetags/friends_tags.py
+++ friends/templatetags/friends_tags.py
@@ -1,35 +1,35 @@
 from django import template
 
-from friends.models import Friendship, FriendshipInvitation, INVITE_STATUS
+from friends.models import Friendship, FriendshipInvitation
 
 
 register = template.Library()
 
 
-class FriendshipStatusNode(template.Node):
-    
+class AreFriendsNode(template.Node):
+    """
+    Returns True if two users are friends, False otherwise.
+    """
     def __init__(self, user1, user2, varname=None):
         self.user1 = user1
         self.user2 = user2
         self.varname = varname
-    
+
     def render(self, context):
         user1 = self.user1.resolve(context)
         user2 = self.user2.resolve(context)
         if Friendship.objects.are_friends(user1, user2):
-            status = "friends"
+            friends = True
         else:
-            status = FriendshipInvitation.objects.invitation_status(user1, user2)
-            if status is not None:
-                status = INVITE_STATUS[int(status)-1][1]
+            friends = False
         if self.varname:
-            context[self.varname] = status
+            context[self.varname] = friends
             return ""
-        return status
+        return friends
 
 
 @register.tag
-def friendship_status(parser, token):
+def are_friends(parser, token):
     bits = token.split_contents()
     tag_name = bits[0]
     bits = bits[1:]
@@ -37,13 +37,13 @@ def friendship_status(parser, token):
         if bits[2] != "as":
             raise template.TemplateSyntaxError("This tag should be used like: "
                 "{%% %s user1 user2 [as varname] %%}" % tag_name)
-        return FriendshipStatusNode(
+        return AreFriendsNode(
             parser.compile_filter(bits[0]),
             parser.compile_filter(bits[1]),
             bits[3]
         )
     elif len(bits) == 2:
-        return FriendshipStatusNode(
+        return AreFriendsNode(
             parser.compile_filter(bits[0]),
             parser.compile_filter(bits[1])
         )
