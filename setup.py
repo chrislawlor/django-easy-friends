0c5e2f1146faa06cd961c666524121d86349ca10
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2012-03-19 06:31:08 +0100

    changelog removed, setup updated

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = "django-friends",
+    name = "django-easy-friends",
     version = "0.2.dev1",
-    description = "friendship, contact and invitation management for the Django web framework",
-    author = "James Tauber",
-    author_email = "jtauber@jtauber.com",
-    url = "http://github.com/jtauber/django-friends/",
+    description = "friendship, friendship invitation and friendship suggestion management for the Django web framework",
+    author = "Maciej Marczewski",
+    author_email = "maciej@marczewski.net.pl",
+    url = "http://github.com/barszczmm/django-easy-friends/",
     packages = find_packages(),
     classifiers = [
         "Development Status :: 3 - Alpha",
