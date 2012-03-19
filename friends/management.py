76de4d3dd0bc6dfcc3920ac314a8ea17a611a818
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2012-03-19 06:35:27 +0100

    added notification about friends removal

--- friends/management.py
+++ friends/management.py
@@ -9,11 +9,14 @@ if friends_settings.FRIENDS_USE_NOTIFICATION_APP and "notification" in settings.
     from notification import models as notification
 
     def create_notice_types(app, created_models, verbosity, **kwargs):
-        notification.create_notice_type("friends_invite", _("Invitation Received"), _("You have received an invitation."), default=1)
-        notification.create_notice_type("friends_invite_sent", _("Invitation Sent"), _("You have sent an invitation."), default=1)
-        notification.create_notice_type("friends_accept", _("Acceptance Received"), _("An invitation you sent has been accepted."), default=1)
-        notification.create_notice_type("friends_accept_sent", _("Acceptance Sent"), _("You have accepted an invitation you received."), default=1)
-        notification.create_notice_type("friends_otherconnect", _("Other Connection"), _("One of your friends has a new friend."), default=1)
+        notification.create_notice_type("friends_invite", _("Invitation received"), _("You have received an invitation."), default=1)
+        notification.create_notice_type("friends_invite_sent", _("Invitation sent"), _("You have sent an invitation."), default=1)
+        notification.create_notice_type("friends_accept", _("Acceptance received"), _("An invitation you sent has been accepted."), default=1)
+        notification.create_notice_type("friends_accept_sent", _("Acceptance sent"), _("You have accepted an invitation you received."), default=1)
+        if friends_settings.NOTIFY_ABOUT_NEW_FRIENDS_OF_FRIEND:
+            notification.create_notice_type("friends_otherconnect", _("Other connection"), _("One of your friends has a new friend."), default=1)
+        if friends_settings.NOTIFY_ABOUT_FRIENDS_REMOVAL:
+            notification.create_notice_type("friends_friend_removed", _("Friend removed"), _("One person was removed from your friends."), default=1)
 
     signals.post_syncdb.connect(create_notice_types, sender=notification)
 else:
--- friends/settings.py
+++ friends/settings.py
@@ -13,3 +13,7 @@ SHOW_FRIENDS_OF_FRIEND = getattr(settings,
 NOTIFY_ABOUT_NEW_FRIENDS_OF_FRIEND = getattr(settings,
                                              'NOTIFY_ABOUT_NEW_FRIENDS_OF_FRIEND',
                                              False)
+
+NOTIFY_ABOUT_FRIENDS_REMOVAL = getattr(settings,
+                                       'NOTIFY_ABOUT_FRIENDS_REMOVAL',
+                                       False)
