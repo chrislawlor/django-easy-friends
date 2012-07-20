9102ca058594a9d28dd5aa237d98de0cf16b4647
Chris Lawlor <lawlor.chris@gmail.com>
2012-07-20 13:51:01 -0400

    Adds admin for FriendLists

--- friends/admin.py
+++ friends/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from friends.models import Friendship, FriendshipInvitation, Blocking
+from friends.models import Friendship, FriendshipInvitation, Blocking, FriendList
 
 
 class FriendshipAdmin(admin.ModelAdmin):
@@ -14,7 +14,15 @@ class FriendshipInvitationAdmin(admin.ModelAdmin):
 class BlockingAdmin(admin.ModelAdmin):
     list_display = ["id", "from_user", "to_user", "added"]
 
+class FriendListAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "owner"]
+    list_display_links = ["id", "title"]
+    filter_horizontal = ["friends"]
+    search_fields = ["^owner__username", "^title"]
+    readonly_fields = ["owner"]
+
 
 admin.site.register(Friendship, FriendshipAdmin)
 admin.site.register(FriendshipInvitation, FriendshipInvitationAdmin)
 admin.site.register(Blocking, BlockingAdmin)
+admin.site.register(FriendList, FriendListAdmin)
